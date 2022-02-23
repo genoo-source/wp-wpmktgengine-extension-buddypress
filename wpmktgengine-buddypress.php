aed241ee6d111dfe44758261404d912cd8ae02c7
Anilkumar18 <32846534+Anilkumar18@users.noreply.github.com>
2022-02-23 21:25:58 +0530

    changed plugin name on the active extension list

--- wpmktgengine-buddypress.php
+++ wpmktgengine-buddypress.php
@@ -29,66 +29,75 @@
  * On activation
  */
 
-register_activation_hook(__FILE__, function(){
+register_activation_hook(__FILE__, function () {
     // Basic extension data
     $fileFolder = basename(dirname(__FILE__));
     $file = basename(__FILE__);
     $filePlugin = $fileFolder . DIRECTORY_SEPARATOR . $file;
     // Activate?
-    $activate = FALSE;
-    $isGenoo = FALSE;
+    $activate = false;
+    $isGenoo = false;
     // Get api / repo
-    if(class_exists('\WPME\ApiFactory') && class_exists('\WPME\RepositorySettingsFactory')){
-        $activate = TRUE;
+    if (
+        class_exists('\WPME\ApiFactory') &&
+        class_exists('\WPME\RepositorySettingsFactory')
+    ) {
+        $activate = true;
         $repo = new \WPME\RepositorySettingsFactory();
         $api = new \WPME\ApiFactory($repo);
-        if(class_exists('\Genoo\Api')){
-            $isGenoo = TRUE;
+        if (class_exists('\Genoo\Api')) {
+            $isGenoo = true;
         }
-    } elseif(class_exists('\Genoo\Api') && class_exists('\Genoo\RepositorySettings')){
-        $activate = TRUE;
+    } elseif (
+        class_exists('\Genoo\Api') &&
+        class_exists('\Genoo\RepositorySettings')
+    ) {
+        $activate = true;
         $repo = new \Genoo\RepositorySettings();
         $api = new \Genoo\Api($repo);
-        $isGenoo = TRUE;
-    } elseif(class_exists('\WPMKTENGINE\Api') && class_exists('\WPMKTENGINE\RepositorySettings')){
-        $activate = TRUE;
+        $isGenoo = true;
+    } elseif (
+        class_exists('\WPMKTENGINE\Api') &&
+        class_exists('\WPMKTENGINE\RepositorySettings')
+    ) {
+        $activate = true;
         $repo = new \WPMKTENGINE\RepositorySettings();
         $api = new \WPMKTENGINE\Api($repo);
     }
     // 1. First protectoin, no WPME or Genoo plugin
-    if($activate == FALSE){
+    if ($activate == false) {
         genoo_wpme_deactivate_plugin(
             $filePlugin,
             'This extension requires Wpmktgengine or Genoo plugin to work with.'
         );
     } else {
-        if(!$api){
+        if (!$api) {
             return;
         }
         // Right on, let's run the tests etc.
         // 2. Second test, can we activate this extension?
         // Active
-        $active = get_option('wpmktengine_extension_forums', NULL);
-        if($isGenoo === TRUE){
-            $active = TRUE;
+        $active = get_option('wpmktengine_extension_forums', null);
+        if ($isGenoo === true) {
+            $active = true;
         }
-        if($active === NULL){
+        if ($active === null) {
             // Oh oh, no value, lets add one
             try {
                 // Might be older package
-                if(method_exists($api, 'getPackageForums')){
+                if (method_exists($api, 'getPackageForums')) {
                     $active = $api->getPackageForums();
                 } else {
-                    $active = FALSE;
+                    $active = false;
                 }
-            } catch (\Exception $e){
-                $active = FALSE;
+            } catch (\Exception $e) {
+                $active = false;
             }
             // Save new value
-            update_option('wpmktengine_extension_forums', $active, TRUE);
+            update_option('wpmktengine_extension_forums', $active, true);
         }
         // 3. Check if we can activate the plugin after all
-        if($active == FALSE){
+        if ($active == false) {
             genoo_wpme_deactivate_plugin(
                 $filePlugin,
                 'This extension is not allowed as part of your package.'
@@ -96,19 +105,17 @@ register_activation_hook(__FILE__, function(){
         } else {
             // 4. After all we can activate, that's great, lets add those calls
             try {
-                $api->setStreamTypes(
-                    array(
-                        array(
-                            'name' => 'started discussion',
-                            'description' => ''
-                        ),
-                        array(
-                            'name' => 'replied in discussion',
-                            'description' => ''
-                        ),
-                    )
-                );
-            } catch(\Exception $e){
+                $api->setStreamTypes([
+                    [
+                        'name' => 'started discussion',
+                        'description' => '',
+                    ],
+                    [
+                        'name' => 'replied in discussion',
+                        'description' => '',
+                    ],
+                ]);
+            } catch (\Exception $e) {
                 // Decide later
             }
         }
@@ -119,66 +126,89 @@ register_activation_hook(__FILE__, function(){
  * Plugin loaded
  */
 
-add_action('wpmktengine_init', function($repositarySettings, $api, $cache){
+add_action(
+    'wpmktengine_init',
+    function ($repositarySettings, $api, $cache) {
+        /**
+         * Started Discussion (name of topic - name of Forum)
+         */
 
-    /**
-     * Started Discussion (name of topic - name of Forum)
-     */
+        add_action(
+            'bp_forums_new_topic',
+            function ($topic_id) use ($api) {
+                // Get user
+                $user = wp_get_current_user();
+                $topic = get_post($topic_id);
+                //$api->putActivityByMail($user->user_email, 'started discussion', '' . $topic->post_title, '', get_permalink($topic->ID));
+            },
+            10,
+            1
+        );
 
-    add_action('bp_forums_new_topic', function($topic_id) use ($api){
-        // Get user
-        $user = wp_get_current_user();
-        $topic = get_post($topic_id);
-        //$api->putActivityByMail($user->user_email, 'started discussion', '' . $topic->post_title, '', get_permalink($topic->ID));
+        /**
+         * Replied in Discussion (name of topic - name of Forum)
+         */
 
-    }, 10, 1);
+        add_action(
+            'bp_forums_new_post',
+            function ($topic_id) use ($api) {
+                // Get user
+                $user = wp_get_current_user();
+                $topic = get_post($topic_id);
+                //$api->putActivityByMail($user->user_email, 'replied in discussion', '' . $topic->post_title, '', get_permalink($topic->ID));
+            },
+            10,
+            1
+        );
 
-    /**
-     * Replied in Discussion (name of topic - name of Forum)
-     */
+        /**
+         * Group created
+         */
 
-    add_action('bp_forums_new_post', function($topic_id) use ($api){
-        // Get user
-        $user = wp_get_current_user();
-        $topic = get_post($topic_id);
-        //$api->putActivityByMail($user->user_email, 'replied in discussion', '' . $topic->post_title, '', get_permalink($topic->ID));
+        add_action(
+            'groups_create_group',
+            function ($groupid, $member, $group) use ($api) {
+                // Get who created it
+                $creator = get_userdata($member->user_id);
+                // ->user_email
+                // ->user_meta
+                $name = $group->name;
+                $id = $group->id;
+                //$creator->user_email . ' created a group ' . $name;
+            },
+            20,
+            3
+        );
 
-    }, 10, 1);
+        /**
+         * User joins group
+         */
 
-    /**
-     * Group created
-     */
+        add_action(
+            'groups_join_group',
+            function ($group_id, $user_id) use ($api) {
+                $user = get_userdata($user_id);
+                $name = new BP_Groups_Group($group_id, []);
+                $name = $name->name;
+            },
+            10,
+            2
+        );
 
-    add_action('groups_create_group', function($groupid, $member, $group) use($api){
-        // Get who created it
-        $creator = get_userdata($member->user_id);
-        // ->user_email
-        // ->user_meta
-        $name = $group->name;
-        $id = $group->id;
-        //$creator->user_email . ' created a group ' . $name;
-    }, 20, 3);
+        /**
+         * User left group
+         */
 
-    /**
-     * User joins group
-     */
-
-    add_action('groups_join_group', function($group_id, $user_id) use ($api){
-        $user = get_userdata($user_id);
-        $name = new BP_Groups_Group($group_id, array());
-        $name = $name->name;
-    }, 10, 2 );
-
-
-    /**
-     * User left group
-     */
-
-    add_action('groups_leave_group', function($group_id, $user_id) use ($api){
-        $user = get_userdata($user_id);
-        $name = new BP_Groups_Group($group_id, array());
-        $name = $name->name;
-    }, 10, 2 );
+        add_action(
+            'groups_leave_group',
+            function ($group_id, $user_id) use ($api) {
+                $user = get_userdata($user_id);
+                $name = new BP_Groups_Group($group_id, []);
+                $name = $name->name;
+            },
+            10,
+            2
+        );
 
         //add_action('groups_promoted_member', '', 10, 2 );
 
@@ -191,7 +221,7 @@ add_action('wpmktengine_init', function($repositarySettings, $api, $cache){
         add_filter(
             'wpmktengine_tools_extensions_widget',
             function ($array) {
-                $array['Buddypress'] =
+                $array['BuddyPress - WPMktgEngine | Genoo Extension'] =
                     '<span style="color:green">Active</span>';
                 return $array;
             },
@@ -206,8 +236,7 @@ add_action('wpmktengine_init', function($repositarySettings, $api, $cache){
 /**
  * Genoo / WPME deactivation function
  */
-if(!function_exists('genoo_wpme_deactivate_plugin')){
-
+if (!function_exists('genoo_wpme_deactivate_plugin')) {
     /**
      * @param $file
      * @param $message
@@ -217,13 +246,18 @@ if(!function_exists('genoo_wpme_deactivate_plugin')){
     function genoo_wpme_deactivate_plugin($file, $message, $recover = '')
     {
         // Require files
-        require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+        require_once ABSPATH . 'wp-admin/includes/plugin.php';
         // Deactivate plugin
         deactivate_plugins($file);
         unset($_GET['activate']);
         // Recover link
-        if(empty($recover)){
-            $recover = '</p><p><a href="'. admin_url('plugins.php') .'">&laquo; ' . __('Back to plugins.', 'wpmktengine') . '</a>';
+        if (empty($recover)) {
+            $recover =
+                '</p><p><a href="' .
+                admin_url('plugins.php') .
+                '">&laquo; ' .
+                __('Back to plugins.', 'wpmktengine') .
+                '</a>';
         }
         // Die with a message
         wp_die($message . $recover);
